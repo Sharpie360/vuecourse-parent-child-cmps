764cff5a92196f9bd845bbd3dc964f0ed1be1ef1
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-01 21:29:07 -0500

    added - course files

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        // props validation as object
+        props: {
+            name: {
+                type: String
+            },
+            resetFn: Function,
+            userAge: Number
+        },
+        methods: {
+            switchName() {
+                return this.name + ' is awesome'
+            },
+            resetName() {
+                this.name = 'Sean';
+                this.$emit('nameWasReset', this.name)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
